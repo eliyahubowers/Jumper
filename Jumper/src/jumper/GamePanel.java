57433da821f83e54eb641a0f812c01e96dfe2d7b
eliyahubowers <league@WTS-IM-6.attlocal.net>
2018-10-21 11:24:50 -0700

    Jumper (IP)

--- Jumper/src/jumper/GamePanel.java
+++ Jumper/src/jumper/GamePanel.java
@@ -1,5 +1,6 @@
 package jumper;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
@@ -15,12 +16,15 @@ import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	final int START_STATE = 0;
-	final int MENU_STATE = 1;
-	final int GAME_STATE = 2;
-	final int END_STATE = 3;
+	final int INSTRUCTION_STATE = 4;
+	final int GAME_STATE = 1;
+	final int END_STATE = 2;
+	
+	int currentState = START_STATE;
 	
 	Font instructFont;
 	Font titleFont;
+	Font titleFontt;
 	Font deathFont;
 	
 	Timer t; 
@@ -29,28 +33,56 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	
 	GamePanel(){
 		titleFont = new Font("Arial",Font.PLAIN,53);
-		instructFont = new Font("Arial",Font.PLAIN,25);
+		titleFontt = new Font("Arial",Font.PLAIN,25);
+		instructFont = new Font("Arial",Font.PLAIN,40);
 		deathFont = new Font("Arial",Font.PLAIN,53);
 		t = new Timer(1000/100,this);
 		go = new GameObject(100,100,100,100,100,0,0);
 	}
 	
 	void updateStartState() {
-		
-	}
-	
-	void updateMenuState() {
-		
-	}
-	
+	}	
+	void updateInstructionState() {
+	}	
 	void updateGameState() {
-		
-	}
-	
+	}	
 	void updateEndState() {
+	}	
+	void drawStartState(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, 500, 800); 
+		g.setFont(titleFont);
+		g.setColor(Color.BLACK);
 		
-	}
-	
+		g.drawString("Welcome To Jumper", 10, 100);
+		
+		g.setFont(titleFontt);
+		
+		g.drawString("Press SPACE for instructions", 85, 400);
+		g.drawString("Press ENTER to start", 130, 500);
+	}	
+	void drawInstructionState(Graphics g) {
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, 500, 800); 
+		g.setFont(instructFont);
+		g.setColor(Color.BLACK);
+		
+		g.drawString("Press", 200, 100);
+		g.drawString("w , s , a , d /", 140, 160);
+		g.drawString("up , down , right , left ", 70, 200);
+		g.drawString("to Move", 170, 260);
+		
+		g.drawString("go on the logs and lily pads", 10, 400);
+		g.drawString("to cross the river safely", 30, 440);
+	}	
+	void drawGameState(Graphics g) {
+		g.setColor(Color.ORANGE);
+		g.fillRect(0, 0, 500, 800); 
+	}	
+	void drawEndState(Graphics g) {
+		g.setColor(Color.YELLOW);
+		g.fillRect(0, 0, 500, 800); 
+	}	
 	void StartGame() {
 		t.start();
 	}
@@ -59,11 +91,29 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	public void actionPerformed(ActionEvent e) {
 		repaint();
 		go.update();
+		if(currentState == START_STATE) {
+			updateStartState();
+		}else if(currentState == INSTRUCTION_STATE){
+            updateInstructionState();
+        }else if(currentState == GAME_STATE){
+            updateGameState();
+        }else if(currentState == END_STATE){
+          	updateEndState();
+        }
 	}
 	@Override
 
 	public void paintComponent(Graphics g){
         go.draw(g);
+        if(currentState == START_STATE) {
+			drawStartState(g);
+		}else if(currentState == INSTRUCTION_STATE){
+            drawInstructionState(g);
+        }else if(currentState == GAME_STATE){
+            drawGameState(g);
+        }else if(currentState == END_STATE){
+          	drawEndState(g);
+        }
     }
 
 	@Override
@@ -78,10 +128,22 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 			go.setymomentum(1);
 		}else if(e.getKeyCode() == KeyEvent.VK_UP||e.getKeyCode() == KeyEvent.VK_W) {
 			go.setymomentum(-1);
-		}else if(e.getKeyCode() == KeyEvent.VK_DOWN||e.getKeyCode() == KeyEvent.VK_S) {
-			go.setymomentum(1);
-		}else if(e.getKeyCode() == KeyEvent.VK_UP||e.getKeyCode() == KeyEvent.VK_W) {
-			go.setymomentum(-1);
+		}else if(e.getKeyCode() == KeyEvent.VK_RIGHT||e.getKeyCode() == KeyEvent.VK_D) {
+			go.setxmomentum(1);
+		}else if(e.getKeyCode() == KeyEvent.VK_LEFT||e.getKeyCode() == KeyEvent.VK_A) {
+			go.setxmomentum(-1);
+		}
+		if(e.getKeyCode() == KeyEvent.VK_ENTER  && currentState != INSTRUCTION_STATE){
+          currentState++;
+		  if(currentState > END_STATE){
+	          currentState = START_STATE;
+		  }
+		}else if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+			if(currentState == START_STATE) {
+				currentState = INSTRUCTION_STATE;
+			}else if(currentState == INSTRUCTION_STATE){
+				currentState = START_STATE;
+			}
 		}
 	}
 
